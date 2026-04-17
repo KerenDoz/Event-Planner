cc73f77e1f145eb299cce8c930df15fc5b7ed71c
IPaskov <paskov.iliyan@gmail.com>
2026-04-17 20:39:24 +0300

    Fixing primary key of Participant model in DbContext.

--- EventPlanner/EventPlanner/Data/ApplicationDbContext.cs
+++ EventPlanner/EventPlanner/Data/ApplicationDbContext.cs
@@ -8,25 +8,39 @@ namespace EventPlanner.Data;
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
+        : base(options) { }
 
     public DbSet<Event> Events { get; set; } = null!;
     public DbSet<Category> Categories { get; set; } = null!;
     public DbSet<Location> Locations { get; set; } = null!;
+    public DbSet<Participant> Participants { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
-        builder.Entity<Category>()
-            .HasIndex(c => c.Name)
-            .IsUnique();
+        builder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
 
-        builder.Entity<Event>()
+        builder.Entity<Participant>().HasKey(p => new { p.UserId, p.EventId });
+
+        builder
+            .Entity<Participant>()
+            .HasOne(p => p.User)
+            .WithMany(u => u.Participants)
+            .HasForeignKey(p => p.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .Entity<Participant>()
+            .HasOne(p => p.Event)
+            .WithMany(e => e.Participants)
+            .HasForeignKey(p => p.EventId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .Entity<Event>()
             .HasOne(e => e.Organizer)
-            .WithMany()
+            .WithMany(u => u.CreatedEvents)
             .HasForeignKey(e => e.OrganizerId)
             .OnDelete(DeleteBehavior.Restrict);
     }
