a25cd76fab5f5349434dfc07e89f0bf39bbef968
KerenDoz <paskov.iliyan@gmail.com>
2026-04-20 11:53:14 +0300

    feat: Add location filtering and date selection to event search functionality

--- EventPlanner/EventPlanner/Controllers/EventsController.cs
+++ EventPlanner/EventPlanner/Controllers/EventsController.cs
@@ -32,7 +32,7 @@ public class EventsController : Controller
             .Include(e => e.Category)
             .Include(e => e.Location);
 
-        if (query.UpcomingOnly && !query.Date.HasValue)
+        if (query.UpcomingOnly)
         {
             eventsQuery = eventsQuery.Where(e => e.StartDate >= DateTime.Now);
         }
@@ -46,15 +46,6 @@ public class EventsController : Controller
         if (query.CategoryId.HasValue)
         {
             eventsQuery = eventsQuery.Where(e => e.CategoryId == query.CategoryId.Value);
-        }
-
-        if (query.Date.HasValue)
-        {
-            var selectedDate = query.Date.Value.Date;
-            var nextDate = selectedDate.AddDays(1);
-
-            eventsQuery = eventsQuery
-                .Where(e => e.StartDate >= selectedDate && e.StartDate < nextDate);
         }
 
         query.Events = await eventsQuery
--- EventPlanner/EventPlanner/Models/Events/EventIndexQueryModel.cs
+++ EventPlanner/EventPlanner/Models/Events/EventIndexQueryModel.cs
@@ -1,16 +1,24 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace EventPlanner.Models.Events;
 
 public class EventIndexQueryModel
 {
     public string? Search { get; set; }
+
     public int? CategoryId { get; set; }
+
+    public int? LocationId { get; set; }
+
     public bool UpcomingOnly { get; set; } = true;
 
     [DataType(DataType.Date)]
     public DateTime? Date { get; set; }
 
     public IEnumerable<SelectListItem> Categories { get; set; } = Enumerable.Empty<SelectListItem>();
+
+    public IEnumerable<SelectListItem> Locations { get; set; } = Enumerable.Empty<SelectListItem>();
+
     public IEnumerable<EventListItemViewModel> Events { get; set; } = Enumerable.Empty<EventListItemViewModel>();
 }
